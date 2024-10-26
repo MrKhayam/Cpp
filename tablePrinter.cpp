cc5b6b4d3f0e0aba80c5de911f5ead149f34e78c
unknown <ijazkhayam@gmail.com>
2024-10-26 19:51:43 +0500

    table printer

--- tablePrinter.cpp
+++ tablePrinter.cpp
@@ -1,19 +1,11 @@
 #include<iostream>
 
-
 int main () {
     int num;
     std::cout << "Enter a Number...";
     std::cin >> num;
-    if(num > 0) {
-        std::cout << "You Entered a Positive Number. " << num;
+    for(int i = 1; i<=10; i++){
+        std::cout << num << "x" << i << "=" << num*i << std::endl;
     }
-    else if(num < 0) {
-        std::cout << "You Entered a Negative Numebr. " << num;
-    }
-    else {
-        std::cout << "You Entered a Number that is Equal to zero. " << num;
-    }
-
     return 0;
 };
