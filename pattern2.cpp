f4e4b80ec56b45de57cf71e346cdc26869dc4e19
unknown <ijazkhayam@gmail.com>
2024-10-27 08:34:25 +0500

    patterns

--- pattern2.cpp
+++ pattern2.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 
-
 int main() {
     int n;
     cout << "Enter a Number...";
     cin >> n;
 
+
     int i = 1;
     while(i<=n){
-        int j = 1;
-        while(j<=n){
+        int j = n;
+        while(j>=1){
             cout << j;
-            j++;
+            j--;
         }
         cout << endl;
         i++;
     }
-
     return 0;
 }
