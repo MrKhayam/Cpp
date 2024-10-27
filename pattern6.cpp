31ac15c80c5099298bfc7660d87ef3d209f6ddf6
unknown <ijazkhayam@gmail.com>
2024-10-27 09:34:43 +0500

    patterns

--- pattern6.cpp
+++ pattern6.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 
 int main () {
-    int num = 1;
     int n;
     cout<< "Enter a Number...";
     cin>> n;
     for (int i = 1; i <= n; i++) {
+        int value = i;
         for (int j = 1; j<=i; j++){
-            cout << num;
-            num++;
+            cout << value;
+            value++;
         }
         cout<< endl;
     }
