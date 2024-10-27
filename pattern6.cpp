1a786fa0a597022f7a0901715e5a3e4be619fb6b
unknown <ijazkhayam@gmail.com>
2024-10-27 11:20:22 +0500

    patterns

--- pattern6.cpp
+++ pattern6.cpp
@@ -7,11 +7,9 @@ int main () {
     int n;
     cout<< "Enter a Number...";
     cin>> n;
-    for (int i = 1; i <= n; i++) {
-        int value = i;
+    for (int i = 0; i <= n; i++) {
         for (int j = 1; j<=i; j++){
-            cout << value;
-            value++;
+            cout << i-j+1;
         }
         cout<< endl;
     }
