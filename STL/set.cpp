14f9b56d9ac46e0e4d0efd1af5b92182765495b8
unknown <ijazkhayam@gmail.com>
2024-11-04 06:01:13 +0500

    stl

--- STL/set.cpp
+++ STL/set.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
-    
+    set<int> a;
+
+    a.insert(1);
+    a.insert(2);
+    a.insert(7);
+    a.insert(3);
+    a.insert(9);
+
+    for (auto i : a) {
+        cout << i << endl;
+    }
+    cout << endl;
+
+    // To get the iterator of set.
+    set<int>::iterator it = a.begin();
+    it++;
+
+    a.erase(it);
+
+    for (auto i : a) {
+        cout << i << endl;
+    }
+    cout << endl;
 }
